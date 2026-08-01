7a730f6b3a7d3219eeb950cdd2626d631058a747
konstantinzakharenko <zaharenkokostyan47@gmail.com>
2026-08-01 22:42:39 +0300

    weather project continuing/server

--- main_for_weather_project.py
+++ main_for_weather_project.py
@@ -1,16 +1,32 @@
 import utils
-from pywebio.input import input
+from pywebio.input import input, input_group
 from pywebio.output import put_text
+from pywebio import start_server
+from pywebio.session import run_js
 
 
 def main():
-    # count = 0
-    while True:
-        city = input("City: ")
-        current_weather = utils.get_weather_info(city)
-        put_text(f"Температура у {city}: {current_weather['temperature']}")
-        # count = count + 1
-        # print(count)
-        print(current_weather)
 
-main()
+    data = input_group(
+        "Запит на погодну погоду",
+        [
+            input(label="City", name="city", required=True),
+            input(label="Email", name="email", required=True),
+            input(label="Name", name="name", required=True),
+        ],
+    )
+    print(data)
+
+    run_js("""
+        setTimeout(() => {window.location.reload();}, 5000);
+    """)
+    # current_weather = utils.get_weather_info(city)
+    # put_text(f"Температура у {city}: {current_weather['temperature']}")
+
+
+start_server(
+    main,
+    host="0.0.0.0",
+    port=8080,
+    debug=True
+)
